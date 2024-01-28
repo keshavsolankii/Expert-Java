a7b096954761b40b8775637d65bd0a8375ae70bf
Keshav <110564015+keshavsolankii@users.noreply.github.com>
2024-01-28 19:16:32 +0530

    Create Main_method_ques.java

---
+++ OCA Java Programs/Main_method_ques.java
@@ -0,0 +1,21 @@
+package MainMethodQues;
+
+//Is it possible to have main method in more than one class in a program?
+//Answer: YES
+
+class B {
+	static public void main(String []s){
+		System.out.println("ghigh");
+	}
+}
+class A{
+	static public void main(String []s){
+		System.out.println("Hello");
+	}
+}
+
+public class MainMethodQues1{
+	public static void main(String[] args) {
+		
+	}
+}
